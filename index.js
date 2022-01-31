ddd81a8ef7a1428eb913b99c9cebc2843389f1d9
jcmtg <96409725+jcmtg@users.noreply.github.com>
2022-01-31 12:14:40 -0800

    running app w/ nodemon

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ import bodyParser from 'body-parser';
 const app = express(); //the entire app is in here.
 const PORT = 5000;
 
-//app.use(bodyParser.json()); //Each app.use(middleware) is called every time a request is sent to the server.
+// app.use(bodyParser.json()); //Each app.use(middleware) is called every time a request is sent to the server.
 app.use(express.json()); //use express's body-parser
 
 app.listen(PORT, () => console.log(`server running on port: http://localhost:${PORT}`));  //ctrl+c to stop the server (in terminal)
 
-//ran node install --save-dev nodeMon, this only puts it locally.
+// ran node install --save-dev nodeMon, this only puts it locally.
+
+// run npm start
+// if you make a change, then nodemon captures this and restarts.
