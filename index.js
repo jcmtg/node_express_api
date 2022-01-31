5aedbd31ae9ef66601fc0cc18bfce71cd62a74f6
jcmtg <96409725+jcmtg@users.noreply.github.com>
2022-01-31 12:06:51 -0800

    use express body parser
    use express body parser

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
-const app = express();
+const app = express(); //the entire app is in here.
 const PORT = 5000;
 
-app.use(bodyParser.json());
+//app.use(bodyParser.json()); //Each app.use(middleware) is called every time a request is sent to the server.
+app.use(express.json()); //use express's body-parser
