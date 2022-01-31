23f7a19ea5ff379e1c3fb52b8cfdd178cce9d9e4
jcmtg <96409725+jcmtg@users.noreply.github.com>
2022-01-31 12:17:23 -0800

    route the homepage.

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const PORT = 5000;
 // app.use(bodyParser.json()); //Each app.use(middleware) is called every time a request is sent to the server.
 app.use(express.json()); //use express's body-parser
 
+//first route
+app.get('/', (req, res) => {
+	console.log('[TEST]!');
+	res.send('Hello from homepage.');
+
+}); //GET request to the root/hoempage., 2nd param is the callback func
+
 app.listen(PORT, () => console.log(`server running on port: http://localhost:${PORT}`));  //ctrl+c to stop the server (in terminal)
 
 // ran node install --save-dev nodeMon, this only puts it locally.
