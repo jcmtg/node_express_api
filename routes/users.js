7f0ccd07b50741c09f856c6c80ad85bfd8a1e810
jcmtg <96409725+jcmtg@users.noreply.github.com>
2022-01-31 19:02:57 -0800

    post stuff

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,32 @@ import express from 'express';
 
 const router = express.Router(); //init our router. similar to app.get() in index.js
 
+const users = [
+	{
+		firstName:"John",
+		lastName:"Doe",
+		age:25
+	},
+	{
+		firstName:"Jane",
+		lastName:"Doe",
+		age:23
+	}
+];
+
 // all routes in here are starting with /users
 router.get('/', (req, res) => {
-	res.send('Hello, in uers.js');
+	console.log(users);
+	res.send(users);
+});
+
+router.post('/', (req, res) => {
+	const user = req.body;
+
+	users.push(user)
+
+
+	res.send('post route reach!');
 });
 
 export default router;
